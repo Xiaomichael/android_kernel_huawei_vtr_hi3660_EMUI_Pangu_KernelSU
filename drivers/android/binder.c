c9b2e41932fe91d68a93f5f73134442244199284
Todd Kjos <tkjos@android.com>
2018-02-07 13:57:37 -0800

    binder: replace "%p" with "%pK"
    commit 8ca86f1639ec5890d400fff9211aca22d0a392eb upstream.
    
    The format specifier "%p" can leak kernel addresses. Use
    "%pK" instead. There were 4 remaining cases in binder.c.
    
    Signed-off-by: Todd Kjos <tkjos@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    [bwh: Backported to 4.9: adjust context]
    Signed-off-by: Ben Hutchings <ben.hutchings@codethink.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -5703,44 +5703,49 @@ binder_defer_work(struct binder_proc *proc, enum binder_deferred_state defer)
 }
 
 static void print_binder_transaction_ilocked(struct seq_file *m,
-					     struct binder_proc *proc,
-					     const char *prefix,
-					     struct binder_transaction *t)
+                                             struct binder_proc *proc,
+                                             const char *prefix,
+                                             struct binder_transaction *t)
 {
-	struct binder_proc *to_proc;
-	struct binder_buffer *buffer = t->buffer;
+        struct binder_proc *to_proc;
+        struct binder_buffer *buffer = t->buffer;
 
-	spin_lock(&t->lock);
-	to_proc = t->to_proc;
-	seq_printf(m,
-		   "%s %d: %pK from %d:%d to %d:%d code %x flags %x pri %d:%d r%d",
-		   prefix, t->debug_id, t,
-		   t->from ? t->from->proc->pid : 0,
-		   t->from ? t->from->pid : 0,
-		   to_proc ? to_proc->pid : 0,
-		   t->to_thread ? t->to_thread->pid : 0,
-		   t->code, t->flags, t->priority.sched_policy,
-		   t->priority.prio, t->need_reply);
-	spin_unlock(&t->lock);
+        spin_lock(&t->lock);
+        to_proc = t->to_proc;
+        seq_printf(m,
+                   "%s %d: %pK from %d:%d to %d:%d code %x flags %x pri %ld r%d",
+                   prefix, t->debug_id, t,
+                   t->from ? t->from->proc->pid : 0,
+                   t->from ? t->from->pid : 0,
+                   to_proc ? to_proc->pid : 0,
+                   t->to_thread ? t->to_thread->pid : 0,
+                   t->code, t->flags, t->priority.prio, t->need_reply);
+        spin_unlock(&t->lock);
 
-	if (proc != to_proc) {
-		/*
-		 * Can only safely deref buffer if we are holding the
-		 * correct proc inner lock for this node
-		 */
-		seq_puts(m, "\n");
-		return;
-	}
+        if (proc != to_proc) {
+                /*
+                 * Can only safely deref buffer if we are holding the
+                 * correct proc inner lock for this node
+                 */
+                seq_puts(m, "\n");
+                return;
+        }
 
-	if (buffer == NULL) {
-		seq_puts(m, " buffer free\n");
-		return;
-	}
-	if (buffer->target_node)
-		seq_printf(m, " node %d", buffer->target_node->debug_id);
-	seq_printf(m, " size %zd:%zd data %pK\n",
-		   buffer->data_size, buffer->offsets_size,
-		   buffer->data);
+        if (t->buffer->target_node)
+                seq_printf(m, " node %d",
+                           t->buffer->target_node->debug_id);
+        seq_printf(m, " size %zd:%zd data %pK\n",
+                   t->buffer->data_size, t->buffer->offsets_size,
+                   t->buffer->data);
+}
+
+static void print_binder_buffer(struct seq_file *m, const char *prefix,
+                                struct binder_buffer *buffer)
+{
+        seq_printf(m, "%s %d: %pK size %zd:%zd %s\n",
+                   prefix, buffer->debug_id, buffer->data,
+                   buffer->data_size, buffer->offsets_size,
+                   buffer->data);
 }
 
 static void print_binder_work_ilocked(struct seq_file *m,
