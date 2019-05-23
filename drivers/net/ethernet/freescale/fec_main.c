d37d298e2331e7adcb9cc6cf952662e33c11337d
Andy Duan <fugang.duan@nxp.com>
2019-05-23 01:55:28 +0000

    net: fec: fix the clk mismatch in failed_reset path
    [ Upstream commit ce8d24f9a5965a58c588f9342689702a1024433c ]
    
    Fix the clk mismatch in the error path "failed_reset" because
    below error path will disable clk_ahb and clk_ipg directly, it
    should use pm_runtime_put_noidle() instead of pm_runtime_put()
    to avoid to call runtime resume callback.
    
    Reported-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Fugang Duan <fugang.duan@nxp.com>
    Tested-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/ethernet/freescale/fec_main.c
+++ drivers/net/ethernet/freescale/fec_main.c
@@ -3492,16 +3492,16 @@ failed_register:
 failed_mii_init:
 failed_irq:
 failed_init:
-	fec_ptp_stop(pdev);
-	if (fep->reg_phy)
-		regulator_disable(fep->reg_phy);
-failed_regulator:
-	clk_disable_unprepare(fep->clk_ipg);
+    fec_ptp_stop(pdev);
+    if (fep->reg_phy)
+        regulator_disable(fep->reg_phy);
+failed_reset:
+    pm_runtime_put_noidle(&pdev->dev);
+    pm_runtime_disable(&pdev->dev);
 failed_clk_ipg:
-	fec_enet_clk_enable(ndev, false);
+    clk_disable_unprepare(fep->clk_ipg);
 failed_clk:
-	if (of_phy_is_fixed_link(np))
-		of_phy_deregister_fixed_link(np);
+    fec_enet_clk_enable(ndev, false);
 failed_phy:
 	of_node_put(phy_node);
 failed_ioremap:
