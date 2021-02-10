6cd9b5cc5f16ce514281996643ee80169cbde5b5
DooHyun Hwang <dh0421.hwang@samsung.com>
2021-02-10 13:59:36 +0900

    mmc: core: Do a power cycle when the CMD11 fails
    commit 147186f531ae49c18b7a9091a2c40e83b3d95649 upstream.
    
    A CMD11 is sent to the SD/SDIO card to start the voltage switch procedure
    into 1.8V I/O. According to the SD spec a power cycle is needed of the
    card, if it turns out that the CMD11 fails. Let's fix this, to allow a
    retry of the initialization without the voltage switch, to succeed.
    
    Note that, whether it makes sense to also retry with the voltage switch
    after the power cycle is a bit more difficult to know. At this point, we
    treat it like the CMD11 isn't supported and therefore we skip it when
    retrying.
    
    Signed-off-by: DooHyun Hwang <dh0421.hwang@samsung.com>
    Link: https://lore.kernel.org/r/20210210045936.7809-1-dh0421.hwang@samsung.com
    Cc: stable@vger.kernel.org
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -1943,14 +1943,11 @@ int mmc_set_signal_voltage(struct mmc_host *host, int signal_voltage, u32 ocr)
 	cmd.flags = MMC_RSP_PRESENT | MMC_RSP_OPCODE | MMC_CMD_AC | MMC_RSP_136;
 
 	err = mmc_wait_for_cmd(host, &cmd, 0);
-	if (err)
-	
-	{
-		if(!strcmp(mmc_hostname(host),"mmc1"))
-		{
-			printk(KERN_ERR "%s:send cmd11 fail,err=%d\n",mmc_hostname(host),err);
-		}
-			return -EAGAIN;
+	if (err) {
+		if (!strcmp(mmc_hostname(host), "mmc1"))
+			printk(KERN_ERR "%s: send cmd11 fail, err=%d\n",
+			       mmc_hostname(host), err);
+		goto power_cycle;
 	}
 	if (!mmc_host_is_spi(host) && (cmd.resp[0] & R1_ERROR))
 		return -EIO;
