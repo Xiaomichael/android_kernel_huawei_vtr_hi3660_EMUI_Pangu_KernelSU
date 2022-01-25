eb5205bacdd7a85c92a20d3fe2088b943c7476f6
Daniel Rosenberg <drosen@google.com>
2022-01-25 14:18:07 +0000

    ion: Protect kref from userspace manipulation
    This separates the kref for ion handles into two components.
    Userspace requests through the ioctl will hold at most one
    reference to the internally used kref. All additional requests
    will increment a separate counter, and the original reference is
    only put once that counter hits 0. This protects the kernel from
    a poorly behaving userspace.
    
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    [d-cagle@codeaurora.org: Resolve style issues]
    Signed-off-by: Dennis Cagle <d-cagle@codeaurora.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/ion/ion-ioctl.c
+++ drivers/staging/android/ion/ion-ioctl.c
@@ -38,6 +38,69 @@ union ion_ioctl_arg {
 	struct ion_map_iommu_data map_iommu;
 };
 
+/* Must hold the client lock */
+static void user_ion_handle_get(struct ion_handle *handle)
+{
+	if (handle->user_ref_count++ == 0)
+		kref_get(&handle->ref);
+}
+
+/* Must hold the client lock */
+static struct ion_handle *user_ion_handle_get_check_overflow(
+	struct ion_handle *handle)
+{
+	if (handle->user_ref_count + 1 == 0)
+		return ERR_PTR(-EOVERFLOW);
+	user_ion_handle_get(handle);
+	return handle;
+}
+
+/* passes a kref to the user ref count.
+ * We know we're holding a kref to the object before and
+ * after this call, so no need to reverify handle.
+ */
+static struct ion_handle *pass_to_user(struct ion_handle *handle)
+{
+	struct ion_client *client = handle->client;
+	struct ion_handle *ret;
+
+	mutex_lock(&client->lock);
+	ret = user_ion_handle_get_check_overflow(handle);
+	ion_handle_put_nolock(handle);
+	mutex_unlock(&client->lock);
+	return ret;
+}
+
+/* Must hold the client lock */
+static int user_ion_handle_put_nolock(struct ion_handle *handle)
+{
+	int ret;
+
+	if (--handle->user_ref_count == 0)
+		ret = ion_handle_put_nolock(handle);
+
+	return ret;
+}
+
+static void user_ion_free_nolock(struct ion_client *client,
+				 struct ion_handle *handle)
+{
+	bool valid_handle;
+
+	WARN_ON(client != handle->client);
+
+	valid_handle = ion_handle_validate(client, handle);
+	if (!valid_handle) {
+		WARN(1, "%s: invalid handle passed to free.\n", __func__);
+		return;
+	}
+	if (handle->user_ref_count == 0) {
+		WARN(1, "%s: User does not have access!\n", __func__);
+		return;
+	}
+	user_ion_handle_put_nolock(handle);
+}
+
 static int validate_ioctl_arg(unsigned int cmd, union ion_ioctl_arg *arg)
 {
 	int ret = 0;
@@ -81,7 +144,7 @@ long ion_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 
 	if (_IOC_SIZE(cmd) > sizeof(data)) {
 		pr_err("%s: cmd size too large!\n", __func__);
- 		return -EINVAL;
+		return -EINVAL;
 	}
 
 	/*
@@ -89,10 +152,10 @@ long ion_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 	 * to do the validate. If there is no write for the ioctl, the
 	 * buffer is cleared
 	 */
-	if (copy_from_user(&data, (void __user *)arg, _IOC_SIZE(cmd))){
+	if (copy_from_user(&data, (void __user *)arg, _IOC_SIZE(cmd))) {
 		pr_err("%s: copy arg failed!\n", __func__);
 		return -EFAULT;
-    }
+	}
 
 	ret = validate_ioctl_arg(cmd, &data);
 	if (ret) {
@@ -121,7 +184,7 @@ long ion_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 				data.allocation.flags);
 			return PTR_ERR(handle);
 		}
-
+		handle = pass_to_user(handle);
 		data.allocation.handle = handle->id;
 		cleanup_handle = handle;
 		break;
@@ -136,7 +199,7 @@ long ion_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 			mutex_unlock(&client->lock);
 			return PTR_ERR(handle);
 		}
-		ion_free_nolock(client, handle);
+		user_ion_free_nolock(client, handle);
 		ion_handle_put_nolock(handle);
 		mutex_unlock(&client->lock);
 		break;
@@ -157,12 +220,12 @@ long ion_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 		mutex_unlock(&client->lock);
 		if (data.fd.fd < 0)
 			ret = data.fd.fd;
-        #ifdef CONFIG_HW_FDLEAK
-                if (ION_IOC_SHARE == cmd)
-                        fdleak_report(FDLEAK_WP_DMABUF, 0);
-                else
-                        fdleak_report(FDLEAK_WP_DMABUF, 1);
-        #endif
+		#ifdef CONFIG_HW_FDLEAK
+		if (ION_IOC_SHARE == cmd)
+			fdleak_report(FDLEAK_WP_DMABUF, 0);
+		else
+			fdleak_report(FDLEAK_WP_DMABUF, 1);
+		#endif
 		break;
 	}
 	case ION_IOC_IMPORT:
@@ -171,14 +234,17 @@ long ion_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 
 		handle = ion_import_dma_buf_fd(client, data.fd.fd);
 		if (IS_ERR(handle)) {
-			pr_err("handle is error %d\n", __LINE__);
 			ret = PTR_ERR(handle);
+		} else {
+			handle = pass_to_user(handle);
+			if (IS_ERR(handle))
+				ret = PTR_ERR(handle);
+			else
+				data.handle.handle = handle->id;
 		}
-		else
-			data.handle.handle = handle->id;
-        #ifdef CONFIG_HW_FDLEAK
-                fdleak_report(FDLEAK_WP_DMABUF, 2);
-        #endif
+		#ifdef CONFIG_HW_FDLEAK
+		fdleak_report(FDLEAK_WP_DMABUF, 2);
+		#endif
 		break;
 	}
 	case ION_IOC_SYNC:
@@ -257,7 +323,10 @@ long ion_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 			pr_err("%s: copy to user failed! cmd: %d\n",
 					__func__, cmd);
 			if (cleanup_handle) {
-				ion_free(client, cleanup_handle);
+				mutex_lock(&client->lock);
+				user_ion_free_nolock(client, cleanup_handle);
+				ion_handle_put_nolock(cleanup_handle);
+				mutex_unlock(&client->lock);
 			}
 			return -EFAULT;
 		}
